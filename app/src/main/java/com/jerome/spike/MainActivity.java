55661e3086e61579031c5922f319def00abef6c1
Jerome <jdc@free.fr>
2016-09-28 13:29:01 +0200

    add brackets UI

--- app/src/main/java/com/jerome/spike/MainActivity.java
+++ app/src/main/java/com/jerome/spike/MainActivity.java
@@ -21,5 +21,14 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        Button button_brackets = (Button) findViewById(R.id.button_main_brackets);
+        button_brackets.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, BracketsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
