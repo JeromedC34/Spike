e95f68a846496629e2be31a0bfb633fec3b970ac
Jerome <jdc@free.fr>
2016-09-28 15:13:17 +0200

    link scramble to UIwq

--- app/src/main/java/com/jerome/spike/ScrambleActivity.java
+++ app/src/main/java/com/jerome/spike/ScrambleActivity.java
@@ -2,6 +2,10 @@ package com.jerome.spike;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class ScrambleActivity extends AppCompatActivity {
 
@@ -9,5 +13,18 @@ public class ScrambleActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scramble);
+
+        final TextView textView = (TextView) findViewById(R.id.text_scramble);
+        final EditText editText = (EditText) findViewById(R.id.edit_text_scramble);
+        Button button = (Button) findViewById(R.id.button_scramble);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Scramble myScramble = new Scramble();
+                String inputText = editText.getText().toString();
+                textView.setText("Get Scramble: " + myScramble.scrambleTextWords(inputText) + "\n");
+            }
+        });
     }
 }
