da0cf45ac0e03344f3df25df9b3200e581250a0e
Jerome <jdc@free.fr>
2016-09-28 15:46:46 +0200

    remove button / palindrome activity (check done after each key pressed)

--- app/src/main/java/com/jerome/spike/PalindromeActivity.java
+++ app/src/main/java/com/jerome/spike/PalindromeActivity.java
@@ -3,8 +3,8 @@ package com.jerome.spike;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -15,25 +15,26 @@ public class PalindromeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_palindrome);
 
-        final TextView textView = (TextView) findViewById(R.id.text_palindrome);
         final EditText editText = (EditText) findViewById(R.id.edit_text_palindrome);
-        Button button = (Button) findViewById(R.id.button_palindrome);
 
-        button.setOnClickListener(new View.OnClickListener() {
+        editText.setOnKeyListener(new View.OnKeyListener() {
             @Override
-            public void onClick(View v) {
-                Palindrome myPalindrome = new Palindrome();
-                String inputText = editText.getText().toString();
-                textView.setText("");
-                textView.append("Is palindrome (" + inputText + ")? " + String.valueOf(myPalindrome.isPalindrome(inputText)) + "\n");
-                textView.append("Reverse (" + inputText + ") : " + myPalindrome.reverse(inputText) + "\n");
-                if (myPalindrome.isPalindrome(inputText)) {
-                    textView.setBackgroundColor(Color.GREEN);
-                } else {
-                    textView.setBackgroundColor(Color.RED);
-                }
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                doAction(editText.getText().toString());
+                return false;
             }
         });
-
+    }
+    protected void doAction(String string){
+        final TextView textView = (TextView) findViewById(R.id.text_palindrome);
+        Palindrome myPalindrome = new Palindrome();
+        textView.setText("");
+        textView.append("Is palindrome (" + string + ")? " + String.valueOf(myPalindrome.isPalindrome(string)) + "\n");
+        textView.append("Reverse (" + string + ") : " + myPalindrome.reverse(string) + "\n");
+        if (myPalindrome.isPalindrome(string)) {
+            textView.setBackgroundColor(Color.GREEN);
+        } else {
+            textView.setBackgroundColor(Color.RED);
+        }
     }
 }
