4584f33d7909bda044b13ffe7f6e334aad77f333
Jerome <jdc@free.fr>
2016-09-28 11:40:18 +0200

    add Palindrome UI

---
+++ app/src/main/java/com/jerome/spike/PalindromeActivity.java
@@ -0,0 +1,13 @@
+package com.jerome.spike;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class PalindromeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_palindrome);
+    }
+}
