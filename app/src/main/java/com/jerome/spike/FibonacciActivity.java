85be55f492ce097c7f9dd7de519acd6f8611d582
Jerome <jdc@free.fr>
2016-09-28 14:33:35 +0200

    link fibonacci to the UI

--- app/src/main/java/com/jerome/spike/FibonacciActivity.java
+++ app/src/main/java/com/jerome/spike/FibonacciActivity.java
@@ -2,6 +2,10 @@ package com.jerome.spike;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class FibonacciActivity extends AppCompatActivity {
 
@@ -9,5 +13,18 @@ public class FibonacciActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fibonacci);
+
+        final TextView textView = (TextView) findViewById(R.id.text_fibonacci);
+        final EditText editText = (EditText) findViewById(R.id.edit_text_fibonacci);
+        Button button = (Button) findViewById(R.id.button_fibonacci);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fibonacci myFibonacci = new Fibonacci();
+                String inputText = editText.getText().toString();
+                textView.setText("Get Fibonacci: " + myFibonacci.get(Integer.valueOf(inputText)) + "\n");
+            }
+        });
     }
 }
