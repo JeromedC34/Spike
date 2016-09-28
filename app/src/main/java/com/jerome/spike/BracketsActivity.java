bf2bae8a4dddb18218f45979c23154100478157b
Jerome <jdc@free.fr>
2016-09-28 13:52:40 +0200

    link Brackets to UI

--- app/src/main/java/com/jerome/spike/BracketsActivity.java
+++ app/src/main/java/com/jerome/spike/BracketsActivity.java
@@ -2,6 +2,10 @@ package com.jerome.spike;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class BracketsActivity extends AppCompatActivity {
 
@@ -9,5 +13,18 @@ public class BracketsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_brackets);
+
+        final TextView textView = (TextView) findViewById(R.id.text_brackets);
+        final EditText editText = (EditText) findViewById(R.id.edit_text_brackets);
+        Button button = (Button) findViewById(R.id.button_brackets);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Brackets myBrackets = new Brackets();
+                String inputText = editText.getText().toString();
+                textView.setText("Check brackets: " + myBrackets.check(inputText) + "\n");
+            }
+        });
     }
 }
